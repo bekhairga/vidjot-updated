13b50ba9cede1247fb737e399ebd30a1091ae448
BekhaIrga <irgashevbehzod@gmail.com>
2021-04-12 15:14:40 -0700

    Added express nodemon and example of routing

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+	res.send('Hello world');
+});
+
+app.get('/about', (req, res) => {
+	res.send('About');
+});
+
 const PORT = 5000;
 
 app.listen(PORT, () => {
