ebc159728689c15d70539f3e76d1e15789557e55
BekhaIrga <irgashevbehzod@gmail.com>
2021-04-12 16:00:15 -0700

    Added models and example of using schemas of mongodb

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 //connecting to mongoose
-
 mongoose
 	.connect('mongodb://localhost/vidjot-dev', {
 		useNewUrlParser: true,
@@ -15,7 +14,9 @@ mongoose
 	.catch((error) => {
 		console.log('Error' + error);
 	});
-
+//Loading Idea model
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
 //using handlebars middleware
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
